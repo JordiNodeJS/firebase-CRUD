0d6c075b4e3e33abcce08b3b7d41f848d7f4fd45
JordiNodeJS <71975008+JordiNodeJS@users.noreply.github.com>
2022-07-11 12:45:26 +0200

    firebase data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from 'react'
+import { db } from './config/firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
+
 function App() {
+  const [users, setUsers] = useState([])
+  const userCollectionRef = collection(db, 'users')
+
+  useEffect(() => {
+    const getUser = async _ => {
+      const data = await getDocs(userCollectionRef)
+      console.log(data)
+    }
+    getUser()
+  }, [])
+
   return (
     <>
       <button className='btn btn-active'>Button</button>
@@ -8,7 +23,6 @@ function App() {
       <button className='btn btn-active btn-ghost'>Button</button>
       <button className='btn btn-active btn-link'>Button</button>
       <hr />
-
     </>
   )
 }
